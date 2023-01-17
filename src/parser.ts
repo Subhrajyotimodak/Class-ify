179975ad6ea81b11f07cf28eb75a0fd42edadd47
Subhrajyotimodak <writer1ani@gmail.com>
2023-01-17 11:54:08 +0530

    parser

--- src/parser.ts
+++ src/parser.ts
@@ -2,8 +2,7 @@ const parseArray = (arr: string[], joiner: string) => arr.join(joiner);
 const parseObject = (obj: { [key: string]: string }, splitter: string, agg: string, joiner: string) => {
   const keys = Object.keys(obj);
   let classes: string[] = [];
-  for (let index = 0; index < keys.length; index++) {
-    const key = keys[index];
+  for (const key of keys) {
     const value = obj[key];
     const subclasses = value.split(splitter);
     const newSubClasses = subclasses.map((clx) => `${key}${agg}${clx}`).join(joiner);
